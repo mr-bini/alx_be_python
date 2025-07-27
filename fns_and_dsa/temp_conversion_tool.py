d3e7bd6693bd99bb8c895911d6b089954b8d9730
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-07-27 23:50:39 +0300

    temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,14 @@
-# Definition of global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
-# Implement conversion functions
 def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
-# User interaction with error handling
 def main():
     try:
         temp_input = float(input("Enter the temperature to convert: "))
