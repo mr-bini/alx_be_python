63da3dbd8869f986ec11b252681966d0799a166f
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-08-01 22:30:20 +0300

    main-0.py

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,10 +2,9 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)  # Starting balance
-
+    account = BankAccount(100)  # Example starting balance
     if len(sys.argv) < 2:
-        print("Usage: python main-0.py <command>:<amount>")
+        print("Usage: python main.py <command>:<amount>")
         print("Commands: deposit, withdraw, display")
         sys.exit(1)
 
